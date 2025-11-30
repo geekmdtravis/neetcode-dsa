6bf1424dd7167099147def07fb4df5be84e22831
geekmdtravis <travis@geekmd.io>
2025-11-30 15:48:37 -0800

    Added unit tests

--- crates/design-linked-list/src/lib.rs
+++ crates/design-linked-list/src/lib.rs
@@ -1,4 +1,6 @@
-type NodeLink = Option<Box<ListNode>>;
+use std::{cell::RefCell, rc::Rc};
+
+type NodeLink = Option<Rc<RefCell<ListNode>>>;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 struct ListNode {
@@ -12,7 +14,9 @@ impl ListNode {
     }
 }
 
+#[derive(Debug)]
 struct MyLinkedList {
+    length: i32,
     head: NodeLink,
     tail: NodeLink,
 }
@@ -25,6 +29,7 @@ impl MyLinkedList {
     #[allow(dead_code)]
     fn new() -> Self {
         MyLinkedList {
+            length: 0,
             head: None,
             tail: None,
         }
@@ -33,51 +38,150 @@ impl MyLinkedList {
     /// If the index is invalid, return -1.
     #[allow(dead_code)]
     fn get(&self, index: i32) -> i32 {
-        let mut curr = match self.head.as_ref() {
-            Some(n) => n,
+        if index < 0 {
+            return -1;
+        }
+
+        let mut curr = match &self.head {
+            Some(ln) => Rc::clone(ln),
             None => {
                 return -1;
             }
         };
 
         for _ in 0..index {
-            curr = match curr.next.as_ref() {
-                Some(n) => n,
+            let next = match &curr.borrow().next {
+                Some(ln) => Rc::clone(ln),
                 None => {
                     return -1;
                 }
             };
+            curr = next;
         }
-        curr.val
+        curr.borrow().val
     }
 
     #[allow(dead_code)]
     fn add_at_head(&mut self, val: i32) {
-        let mut new_node = ListNode::new(val);
-        new_node.next = self.head.take();
-        self.head = Some(Box::new(new_node));
+        let mut new = ListNode::new(val);
+        if self.length == 0 {
+            let onlynode = &Rc::new(RefCell::new(new));
+            self.head = Some(Rc::clone(onlynode));
+            self.tail = Some(Rc::clone(onlynode));
+            self.length = self.length + 1;
+            return;
+        };
+        let head = match &self.head {
+            Some(ln) => Rc::clone(ln),
+            None => {
+                return;
+            }
+        };
+        new.next = Some(head);
+        self.head = Some(Rc::new(RefCell::new(new)));
+        self.length = self.length + 1;
     }
 
     #[allow(dead_code)]
     fn add_at_tail(&mut self, val: i32) {
-        let new = Box::new(ListNode::new(val));
+        let new = Rc::new(RefCell::new(ListNode::new(val)));
         if let Some(tail) = self.tail.as_mut() {
-            tail.next = Some(new);
-            self.tail = new;
+            tail.borrow_mut().next = Some(new.clone());
+            self.tail = Some(new);
         } else {
-            self.head = new;
-            self.tail = new;
+            self.head = Some(new.clone());
+            self.tail = Some(new);
         }
+        self.length = self.length + 1;
     }
 
     #[allow(dead_code)]
     fn add_at_index(&mut self, index: i32, val: i32) {
-        todo!("implement")
+        let last_index = self.length - 1;
+
+        if index > last_index {
+            return;
+        }
+
+        if index == 0 {
+            self.add_at_head(val);
+            return;
+        }
+
+        if index == last_index {
+            self.add_at_tail(val);
+            return;
+        }
+
+        let mut curr = match &self.head {
+            Some(ln) => Rc::clone(ln),
+            None => {
+                return;
+            }
+        };
+
+        for _ in 0..index + 1 {
+            let next = match &curr.borrow().next {
+                Some(ln) => Rc::clone(ln),
+                None => {
+                    return;
+                }
+            };
+            curr = next;
+        }
+
+        let right = match &curr.borrow().next {
+            Some(ln) => Rc::clone(ln),
+            None => {
+                return;
+            }
+        };
+
+        let mut new = ListNode::new(val);
+        new.next = Some(right);
+
+        let left = curr;
+        left.borrow_mut().next = Some(Rc::new(RefCell::new(new)));
+        self.length = self.length + 1;
     }
 
     #[allow(dead_code)]
-    fn delete_at_index(&self, index: i32) {
-        todo!("Implement")
+    fn delete_at_index(&mut self, index: i32) {
+        if index < 0 || index > self.length - 1 {
+            return;
+        }
+
+        let mut curr = match &self.head {
+            Some(ln) => Rc::clone(ln),
+            None => {
+                return;
+            }
+        };
+
+        let mut left = None;
+        for _ in 0..index {
+            let next = match &curr.borrow().next {
+                Some(ln) => Rc::clone(ln),
+                None => {
+                    return;
+                }
+            };
+            left = Some(curr);
+            curr = next;
+        }
+
+        let right = match &curr.borrow().next {
+            Some(ln) => Rc::clone(ln),
+            None => {
+                return;
+            }
+        };
+
+        curr.borrow_mut().next = None;
+        if let Some(left) = left {
+            left.borrow_mut().next = Some(right);
+        };
+        self.length = self.length - 1;
     }
 }
 
@@ -96,7 +200,81 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(1, 4);
+    fn it_adds_at_head() {
+        let mut linked_list = MyLinkedList::new();
+        linked_list.add_at_head(1);
+        assert_eq!(linked_list.get(0), 1);
+        linked_list.add_at_head(2);
+        linked_list.add_at_head(3);
+        assert_eq!(linked_list.get(0), 3);
+        assert_eq!(linked_list.get(1), 2);
+        assert_eq!(linked_list.get(2), 1);
+    }
+
+    #[test]
+    fn it_adds_at_tail() {
+        let mut linked_list = MyLinkedList::new();
+        linked_list.add_at_tail(1);
+        assert_eq!(linked_list.get(0), 1);
+        linked_list.add_at_tail(2);
+        linked_list.add_at_tail(3);
+        assert_eq!(linked_list.get(0), 1);
+        assert_eq!(linked_list.get(1), 2);
+        assert_eq!(linked_list.get(2), 3);
+    }
+
+    #[test]
+    fn it_deletes() {
+        let mut linked_list = MyLinkedList::new();
+        linked_list.add_at_head(1);
+        println!("{:?}", linked_list);
+        linked_list.add_at_index(0, 2);
+        println!("{:?}", linked_list);
+        linked_list.add_at_index(0, 3);
+        println!("{:?}", linked_list);
+
+        assert_eq!(linked_list.get(0), 3);
+        assert_eq!(linked_list.get(1), 2);
+        assert_eq!(linked_list.get(2), 1);
+
+        linked_list.delete_at_index(1);
+        println!("{:?}", linked_list);
+        assert_eq!(linked_list.get(0), 3);
+        assert_eq!(linked_list.get(1), 1);
+    }
+
+    #[test]
+    fn it_adds_at_index_0() {
+        let mut linked_list = MyLinkedList::new();
+        linked_list.add_at_head(1);
+        linked_list.add_at_index(0, 2);
+        assert_eq!(linked_list.get(0), 2);
+        assert_eq!(linked_list.get(1), 1);
+    }
+
+    #[test]
+    fn it_adds_at_index_last() {
+        let mut linked_list = MyLinkedList::new();
+        linked_list.add_at_head(1);
+        linked_list.add_at_head(0);
+        linked_list.add_at_index(1, 2);
+
+        assert_eq!(linked_list.get(0), 0);
+        assert_eq!(linked_list.get(1), 2);
+        assert_eq!(linked_list.get(2), 1);
+    }
+
+    #[test]
+    fn it_adds_at_index_middle() {
+        let mut linked_list = MyLinkedList::new();
+        linked_list.add_at_head(3);
+        linked_list.add_at_head(2);
+        linked_list.add_at_head(1);
+        linked_list.add_at_index(1, 4);
+
+        assert_eq!(linked_list.get(0), 1);
+        assert_eq!(linked_list.get(1), 4);
+        assert_eq!(linked_list.get(2), 2);
+        assert_eq!(linked_list.get(3), 3);
     }
 }
