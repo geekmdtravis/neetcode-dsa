e9ecf7bd51877b336dc90d6936b54f79bf1a7d3e
geekmdtravis <travis@geekmd.io>
2025-12-01 07:06:33 -0800

    Removed unused unit tests

--- crates/reverse-linked-list/src/lib.rs
+++ crates/reverse-linked-list/src/lib.rs
@@ -31,27 +31,3 @@ impl Solution {
         new_head
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let n3 = Some(Box::new(ListNode::new(1)));
-        let n2 = Some(Box::new(ListNode {
-            val: 2,
-            next: n3.clone(),
-        }));
-        let n1 = Some(Box::new(ListNode {
-            val: 3,
-            next: n2.clone(),
-        }));
-
-        let reversed = Solution::reverse_list(n3.clone()).unwrap();
-
-        assert_eq!(reversed.val, n3.unwrap().val);
-        assert_eq!(reversed.next.clone().unwrap().val, n2.unwrap().val);
-        assert_eq!(reversed.next.unwrap().next.unwrap().val, n1.unwrap().val);
-    }
-}
