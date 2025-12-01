cf0f7f1b19d298e59e5e61c80b4ffacb72d8d173
geekmdtravis <travis@geekmd.io>
2025-12-01 04:53:32 -0800

    Started new students unable to eat challenge

---
+++ crates/num-stud-unable-eat-lunch/src/lib.rs
@@ -0,0 +1,16 @@
+struct Solution;
+
+impl Solution {
+    pub fn count_students(students: Vec<i32>, sandwiches: Vec<i32>) -> i32 {
+        todo!()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2, 4);
+    }
+}
