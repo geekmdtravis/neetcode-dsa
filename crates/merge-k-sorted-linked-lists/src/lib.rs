f27c5c6ad723b1700c6b96e3446311161514d4ff
geekmdtravis <travis@geekmd.io>
2025-12-03 04:46:44 -0800

    starting merge k sorted linked lists

---
+++ crates/merge-k-sorted-linked-lists/src/lib.rs
@@ -0,0 +1,62 @@
+#[allow(dead_code)]
+struct Solution;
+
+/// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[allow(dead_code)]
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut node_a1 = ListNode::new(11);
+        let mut node_a2 = ListNode::new(20);
+        let node_a3 = ListNode::new(33);
+        node_a2.next = Some(Box::new(node_a3));
+        node_a1.next = Some(Box::new(node_a2));
+
+        let mut node_b1 = ListNode::new(0);
+        let mut node_b2 = ListNode::new(2);
+        let mut node_b3 = ListNode::new(10);
+        let node_b4 = ListNode::new(30);
+        node_b3.next = Some(Box::new(node_b4));
+        node_b2.next = Some(Box::new(node_b3));
+        node_b1.next = Some(Box::new(node_b2));
+
+        let mut node_c1 = ListNode::new(3);
+        let mut node_c2 = ListNode::new(42);
+        let mut node_c3 = ListNode::new(105);
+        let mut node_c4 = ListNode::new(136);
+        let node_c5 = ListNode::new(1009);
+        node_c4.next = Some(Box::new(node_c5));
+        node_c3.next = Some(Box::new(node_c4));
+        node_c2.next = Some(Box::new(node_c3));
+        node_c1.next = Some(Box::new(node_c2));
+
+        let list = vec![node_a1, node_b1, node_c1];
+
+        println!("{:?}", list);
+
+        assert_eq!(1, 4);
+    }
+}
